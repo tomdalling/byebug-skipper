86ff990ff688f14b9daa7ff7510bb83c24070fb7
Tom Dalling <tom@tomdalling.com>
2021-06-07 13:50:20 +1000

    refactor: refactoring/commenting hacks

--- lib/byebug/skipper/pry.rb
+++ lib/byebug/skipper/pry.rb
@@ -2,51 +2,51 @@ require_relative '../skipper'
 require 'pry-byebug'
 
 module Byebug::Skipper::Pry
-  # All the Pry command classes are implemented the same
-  %w[ups downs steps finishs].each do |command_name|
-    command_class = Class.new(Pry::ClassCommand) do
-      include ::PryByebug::Helpers::Navigation
-
-      match command_name
-      group "Byebug Skipper"
-      description "Same as Byebug's `#{command_name.chomp('s')}` command but skips garbage frames (e.g. from gems)"
-
-      def process
-        PryByebug.check_file_context(target)
-        breakout_navigation command_name
-      end
-    end
-
-    const_set "#{command_name.capitalize}Command", command_class
-    ::Pry::Commands.add_command(command_class)
-  end
+  COMMAND_NAMES = %w[ups downs steps finishs].freeze
+  COMMANDS_THAT_CONTINUE = %w[steps finishs].freeze
 end
 
+# All the Pry command classes are implemented the same
+Byebug::Skipper::Pry::COMMAND_NAMES.each do |command_name|
+  command_class = Class.new(Pry::ClassCommand) do
+    include ::PryByebug::Helpers::Navigation
 
-# Time 2 get nasty
-module Byebug::Skipper::PryProcessorHacks
-  # this needs to be overriden because there is a hard-coded whitelist of
-  # `action` values inside, and I need to add custom actions.
+    match command_name
+    group "Byebug Skipper"
+    description "Same as Byebug's `#{command_name.chomp('s')}` command but skips garbage frames (e.g. from gems)"
+
+    def process
+      PryByebug.check_file_context(target)
+      # This comes from pry-byebug, and we need to monkey patch it (see below)
+      # to get these custom command names to work properly.
+      breakout_navigation(command_name)
+    end
+  end
+
+  Byebug::Skipper::Pry.const_set("#{command_name.capitalize}Command", command_class)
+  Pry::Commands.add_command(command_class)
+end
+
+# This is a monkey patch for Byebug::PryProcessor in order to add extra Byebug
+# commands to Pry. This needs to be overriden because there is a hard-coded
+# whitelist of `action` values inside, and I need to add custom actions.
+module Byebug::Skipper::Pry::ProcessorHacks
   def perform(action, options = {})
-    return if action.nil?
-    send("perform_#{action}", options)
-  end
+    # If it's not one of our commands, short circuit and use the typical
+    # behaviour.
+    return super unless Byebug::Skipper::Pry::COMMAND_NAMES.include?(action.to_s)
 
-  # delegate the implementations straight to the byebug command objects
-  {
-    'ups' => true,
-    'downs' => true,
-    'steps' => false,
-    'finishs' => false,
-  }.each do |command_name, should_resume|
-    define_method("perform_#{command_name}") do |_options|
-      Byebug::Skipper.const_get("#{command_name.capitalize}Command")
-        .new(self, command_name)
-        .execute
+    # Call the Byebug command objects directly. This seems kind of fragile to
+    # me, but I don't see any better options.
+    Byebug::Skipper.const_get("#{command_name.capitalize}Command")
+      .new(self, command_name)
+      .execute
 
-      resume_pry if should_resume
+    unless Byebug::Skipper::Pry::COMMANDS_THAT_CONTINUE.include?(command_name)
+      # This shows the REPL again, preventing execution from continuing.
+      resume_pry
     end
   end
 end
 
-Byebug::PryProcessor.prepend(Byebug::Skipper::PryProcessorHacks)
+Byebug::PryProcessor.prepend(Byebug::Skipper::Pry::ProcessorHacks)
--- lib/byebug/skipper/steps_command.rb
+++ lib/byebug/skipper/steps_command.rb
@@ -17,30 +17,36 @@ module Byebug::Skipper
       HackyProcessor.hack_into(context)
     end
 
-    class HackyProcessor < SimpleDelegator
-      def self.hack_into(context)
-        hack = new(context.send(:processor))
-        context.instance_variable_set(:@processor, hack)
-        hack.skip_this_frame!
-      end
+    private
 
-      def unhack!
-        context.instance_variable_set(:@processor, __getobj__)
-      end
+      # We need to hijack the #at_line method of the processor, to prevent it
+      # from running the REPL when we want it to skip that frame. Sneakily
+      # replaces the processor with this wrapped/delegated version, and then
+      # unwraps/undelegates when no longer necessary.
+      class HackyProcessor < SimpleDelegator
+        def self.hack_into(context)
+          hack = new(context.send(:processor))
+          context.instance_variable_set(:@processor, hack)
+          hack.skip_this_frame!
+        end
 
-      def skip_this_frame!
-        context.step_into(1, context.frame.pos)
-        proceed!
-      end
+        def unhack!
+          context.instance_variable_set(:@processor, __getobj__)
+        end
 
-      def at_line
-        if Byebug::Skipper.skip?(context.location)
-          skip_this_frame!
-        else
-          super
-          unhack!
+        def skip_this_frame!
+          context.step_into(1, context.frame.pos)
+          proceed!
+        end
+
+        def at_line
+          if Byebug::Skipper.skip?(context.location)
+            skip_this_frame! # don't stop, keep running
+          else
+            unhack!
+            super # will stop and show REPL
+          end
         end
       end
-    end
   end
 end
